185f188c69fab1d834626ebbc85c37475fc3523f
Hyerim Yu <zucco2420@inu.ac.kr>
2025-12-15 21:50:12 +0900

    complete seed_api

--- seed_api.py
+++ seed_api.py
@@ -1,34 +1,38 @@
 import requests
 import sqlite3
 from datetime import datetime
+import xml.etree.ElementTree as ET
 
-#파일, 커서 생성
+# ------------------------------
+# DB 연결
+# ------------------------------
 conn = sqlite3.connect("emergency.db")
 cursor = conn.cursor()
 
-#------------------------------
-#openapi 데이터 요청
-#------------------------------
-
-
+# ------------------------------
+# OpenAPI 요청
+# ------------------------------
 url = "http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEmrrmRltmUsefulSckbdInfoInqire"
+
 params = {
     "serviceKey": "54a210e62143f356fa795088187d6e979f466fb935ce200d83ee264183b0e084",
     "STAGE1": "서울특별시",
     "STAGE2": "강남구",
     "pageNo": 1,
-    "numOfRows": 100,
-    "resultType": "json"
+    "numOfRows": 100
 }
 
 res = requests.get(url, params=params)
-data = res.json()
-items = data['response']['body']['items']['item']
 
-#------------------------------
-#int 변환 함수
-#------------------------------
+# ------------------------------
+# XML 파싱
+# ------------------------------
+root = ET.fromstring(res.text)
+items = root.findall(".//item")
 
+# ------------------------------
+# int 변환 함수
+# ------------------------------
 def to_int(v, default=0):
     if v is None:
         return default
@@ -40,57 +44,52 @@ def to_int(v, default=0):
     except:
         return default
 
-#------------------------------
-#DB에 데이터 저장
-#------------------------------
-
-
+# ------------------------------
+# DB 저장
+# ------------------------------
 for item in items:
+    hpid = item.findtext("hpid")
+    dutyname = item.findtext("dutyName")
+    dutytel3 = item.findtext("dutyTel3")
+
+    hvec = to_int(item.findtext("hvec"))
+    hvcc = to_int(item.findtext("hvcc"))
+    hvncc = to_int(item.findtext("hvncc"))
+    hvccc = to_int(item.findtext("hvccc"))
+    hvicc = to_int(item.findtext("hvicc"))
+
+    # 병원 기본 정보
     cursor.execute(
         """
         INSERT OR IGNORE INTO HOSPITAL
         (hpid, dutyname, stage1, stage2, dutytel3)
         VALUES (?, ?, ?, ?, ?)
         """,
-        (
-            item.get("hpid"),
-            item.get("dutyName"),
-            params["STAGE1"],
-            params["STAGE2"],
-            item.get("dutyTel3")
-        )
+        (hpid, dutyname, params["STAGE1"], params["STAGE2"], dutytel3)
     )
 
+    # 응급실 병상 정보
     cursor.execute(
         """
         INSERT INTO BedStatus
         (hpid, hvec, hvidate)
         VALUES (?, ?, ?)
         """,
-        (
-            item.get("hpid"),
-            to_int(item.get("hvec")),
-            datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        )
+        (hpid, hvec, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     )
 
+    # 중증질환 병상 정보
     cursor.execute(
         """
         INSERT OR REPLACE INTO SevereCare
         (hpid, hvcc, hvncc, hvccc, hvicc)
         VALUES (?, ?, ?, ?, ?)
         """,
-        (
-            item.get("hpid"),
-            to_int(item.get("hvcc")),
-            to_int(item.get("hvncc")),
-            to_int(item.get("hvccc")),
-            to_int(item.get("hvicc"))
-        )
+        (hpid, hvcc, hvncc, hvccc, hvicc)
     )
 
-
-
-
+# ------------------------------
+# 종료
+# ------------------------------
 conn.commit()
 conn.close()
